4c8bf6680f764eee52d0895c0b3d1558a1c9d694
asunder <trashemail3301@gmail.com>
2026-06-11 18:08:00 -0400

    Add styling optimizations to prod

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,28 @@
 import { merge } from "webpack-merge";
 import common from "./webpack.common.js";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import CssMinimizerPlugin from "css-minimizer-webpack-plugin";
 
 export default merge(common, {
     mode: "production",
     devtool: "source-map",
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "./src/styles.css",
+        }),
+    ],
+    optimization: {
+        minimizer: [
+            '...',
+            new CssMinimizerPlugin(),
+        ],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+            },
+        ],
+    },
 });
