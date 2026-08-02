4fa4900649637e2b54081f7181a76384717b173b
asunder <trashemail3301@gmail.com>
2026-08-02 13:37:57 -0400

    chore(eslint): update rules for react

--- eslint.config.js
+++ eslint.config.js
@@ -2,13 +2,15 @@ import js from "@eslint/js";
 import globals from "globals";
 import { defineConfig } from "eslint/config";
 import eslintConfigPrettier from "eslint-config-prettier";
+import eslintReact from "@eslint-react/eslint-plugin";
+import reactHooksPlugin from "eslint-plugin-react-hooks";
 
 export default defineConfig([
   {
     ignores: ["dist/**", "node_modules/**", "webpack.*.js", ".git"],
   },
   {
-    files: ["src/**/*.js"],
+    files: ["src/**/*.{js,jsx}"],
     languageOptions: {
       ecmaVersion: "latest",
       sourceType: "module",
@@ -17,8 +19,13 @@ export default defineConfig([
         ...globals.jest,
       },
     },
+    plugins: {
+      "react-hooks": reactHooksPlugin,
+    },
+    ...eslintReact.configs.recommended,
     rules: {
       ...js.configs.recommended.rules,
+      ...reactHooksPlugin.configs.recommended.rules,
       "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
       "no-console": "off",
       eqeqeq: ["error", "always"],
