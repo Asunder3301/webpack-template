0eab8b817b00847d223e4c0c3a67ede925090a7e
asunder <trashemail3301@gmail.com>
2026-08-02 13:52:55 -0400

    chore(jest): ensure Jest extracts .jsx code

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,9 @@
 export default {
+  testEnvironment: "jsdom",
+  moduleFileExtensions: ["js", "jsx", "json", "node"],
+  transform: {
+    "^.+\\.(js|jsx)$": "babel-jest",
+  },
   moduleNameMapper: {
     "\\.(css)$": "./src/styleMock.js",
   },
