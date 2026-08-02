929b0b851fc69dd6a6a475b2e4ee023f674b0b30
asunder <trashemail3301@gmail.com>
2026-08-02 13:47:50 -0400

    chore(webpack): include .jsx files in entry/resolution

--- webpack.common.js
+++ webpack.common.js
@@ -2,11 +2,14 @@ import path from "node:path";
 import HtmlWebpackPlugin from "html-webpack-plugin";
 
 export default {
-  entry: "./src/index.js",
+  entry: "./src/index.jsx",
   output: {
     filename: "main.js",
     path: path.resolve(import.meta.dirname, "dist"),
     clean: true,
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
   },
   plugins: [
     new HtmlWebpackPlugin({
@@ -20,7 +23,14 @@ export default {
         use: ["html-loader"],
       },
       {
-        test: /\.(png|svg|jpgh|jpeg|gif)$/i,
+        test: /\.(js|jsx)$/i,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: "asset/resource",
       },
     ],
