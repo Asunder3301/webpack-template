02b0abd56f1c2e6cbc277c9e9f7abdfcf24352b9
asunder <trashemail3301@gmail.com>
2026-08-08 12:08:33 -0400

    style(jest): fix indentation

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,9 @@
 export default {
   testEnvironment: "jsdom",
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.{js,jsx}'],
-  coverageDirectory: 'coverage',
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  collectCoverageFrom: ["src/**/*.{js,jsx}"],
+  coverageDirectory: "coverage",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   moduleFileExtensions: ["js", "jsx", "json", "node"],
   transform: {
     "^.+\\.(js|jsx)$": "babel-jest",
