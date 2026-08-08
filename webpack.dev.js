190f6be2a90522006cfc69fd26cec46eec88c615
asunder <trashemail3301@gmail.com>
2026-08-08 12:42:29 -0400

    chore(css-loader): enable css modules (.module.css)

--- webpack.dev.js
+++ webpack.dev.js
@@ -20,7 +20,18 @@ export default merge(common, {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: [
+          "style-loader", 
+          {
+            loader: "css-loader",
+            options: {
+              modules: {
+                auto: true,
+                localIdentName: "[name]__[local]--[hash:base64:5]",
+              }
+            }
+          }
+        ],
       },
     ],
   },
