48a8fa84937a9799e74765c1ac54129900b677a2
asunder <trashemail3301@gmail.com>
2026-08-08 12:06:31 -0400

    chore(eslint): config jest and import routing rules

--- eslint.config.js
+++ eslint.config.js
@@ -4,11 +4,14 @@ import { defineConfig } from "eslint/config";
 import eslintConfigPrettier from "eslint-config-prettier";
 import eslintReact from "@eslint-react/eslint-plugin";
 import reactHooksPlugin from "eslint-plugin-react-hooks";
+import jestPlugin from "eslint-plugin-jest";
+import importPlugin from "eslint-plugin-import-x"; // Modern, fast version of eslint-plugin-import
 
 export default defineConfig([
   {
     ignores: ["dist/**", "node_modules/**", "webpack.*.js", ".git"],
   },
+  jestPlugin.configs["flat/recommended"],
   eslintReact.configs.recommended,
   {
     files: ["src/**/*.{js,jsx}"],
@@ -27,15 +30,25 @@ export default defineConfig([
     },
     plugins: {
       "react-hooks": reactHooksPlugin,
+      "import-x": importPlugin,
     },
     rules: {
+      // Base JavaScript recommended rules
       ...js.configs.recommended.rules,
       "react-hooks/rules-of-hooks": "error",
       "react-hooks/exhaustive-deps": "warn",
-
       "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
       "no-console": "off",
       eqeqeq: ["error", "always"],
+      "no-underscore-dangle": 0,
+      "import-x/extensions": [
+        "error",
+        "ignorePackages",
+        {
+          js: "always",
+          jsx: "always",
+        },
+      ],
     },
   },
   eslintConfigPrettier,
